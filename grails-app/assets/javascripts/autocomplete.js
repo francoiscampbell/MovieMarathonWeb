03f603e75409bac64f6b8c8e70be273b8fcde0aa
Francois Campbell <campbell.francois@gmail.com>
2016-05-22 14:08:15 -0400

    Simplified geocoding and removed auto-proceed on place selection to allow date selection

--- grails-app/assets/javascripts/autocomplete.js
+++ grails-app/assets/javascripts/autocomplete.js
@@ -2,19 +2,13 @@
  * Created by francois on 15-08-01.
  */
 
-var autocomplete;
-var submitReady = false;
-
 window.onload = function () {
-    var options = {
+    var autocompleteOptions = {
         bounds: null
     };
-    var input = document.getElementById('pac-place');
-    autocomplete = new google.maps.places.Autocomplete(input, options);
-    google.maps.event.addListener(autocomplete, 'place_changed', function () {
-        $('#form-postcode').submit();
-    });
-    $('#form-postcode').submit(processForm);
+    var pacPlaceInput = document.getElementById('pac-place');
+    var autocomplete = new google.maps.places.Autocomplete(pacPlaceInput, autocompleteOptions);
+    google.maps.event.addListener(autocomplete, 'place_changed', geocodeAddress);
 };
 
 function geocodeAddress() {
@@ -26,7 +20,6 @@ function geocodeAddress() {
         if (geocodeStatus === google.maps.GeocoderStatus.OK) {
             var location = geocodeResults[0].geometry.location;
             setLatLng(location.lat(), location.lng());
-            $('#form-postcode').submit();
         } else {
             alert('Please choose a place');
             resetLatLng();
@@ -37,26 +30,10 @@ function geocodeAddress() {
 function setLatLng(lat, lng) {
     $('#lat').val(lat);
     $('#lng').val(lng);
-    submitReady = true;
 }
 
 function resetLatLng() {
     $('#lat').val('');
     $('#lng').val('');
-    submitReady = false;
 }
 window.onunload = resetLatLng;
-
-function processForm() {
-    if (submitReady) {
-        return true;
-    }
-    var place = autocomplete.getPlace();
-    if (place && place.geometry) {
-        var location = place.geometry.location;
-        setLatLng(location.lat(), location.lng());
-        return true;
-    }
-    geocodeAddress();
-    return false;
-}
